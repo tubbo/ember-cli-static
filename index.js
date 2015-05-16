e28018e181f798ba11197f1ed94330e9a2e39b29
Tom Scott <tscott@weblinc.com>
2015-05-16 15:58:22 -0400

    Add pages.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* jshint node: true */
 'use strict';
 
+var StaticCompiler = require('./lib/compiler');
+
 module.exports = {
-  name: 'ember-cli-static'
+  name: 'ember-cli-static',
+
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+    target.registry.add('html', new StaticCompiler());
+  }
 };
